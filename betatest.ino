165c3d3fcc114b0d83ab9e3768ca07f0a4f18f7f
201524495 <49744580+201524495@users.noreply.github.com>
2019-06-21 21:50:26 +0900

    Update betatest.ino

--- betatest.ino
+++ betatest.ino
@@ -2,95 +2,17 @@
 #include <RTClib.h>
 #include<LiquidCrystal.h>
 #include <Servo.h>
+
 #define PIN_GATE_IN 8 // @DB chekcer(red one) original : 2
 #define IRQ_GATE_IN 9 // @DB checker original : 0
 #define PIN_LED_OUT 10 //  @DB checker original : 13
 #define PIN_ANALOG_IN A1 // @DB checker original : A0
 #define melodyPin 7 // @melodypin
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
 #define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
+#define NOTE_C7  2093
 #define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
+#define NOTE_G6  1568
+
 int melody[] = {
   NOTE_E7, NOTE_E7, 0, NOTE_E7,
   0, NOTE_C7, NOTE_E7, 0,
@@ -175,7 +97,7 @@ void Speaker(int sound, unsigned long interval ){
         lcd.setCursor(0,0);
         lcd.print("Speaker on / off");
         if(interval <= 50){
-        k = 1;
+        k = 0;
         lcd.setCursor(0,1);
         lcd.print("Speaker off     ");
         }
@@ -268,7 +190,7 @@ void Motor(int go, int mot, int later, unsigned long time_current, int k){ //모
         lcd.setCursor(0,1);
          lcd.print("  food : 100 g  ");
 
-        int delayvalue = 3000;  //later * 60 * 1000
+        int delayvalue = 3000;  // 3000은 임시값, 실제값 : later * 60 * 1000
         static int seconds= delayvalue/1000;
         lcd.setCursor(0,0);
         lcd.print(" seconds : "+String(seconds)+"   ");
@@ -299,7 +221,7 @@ void Motor(int go, int mot, int later, unsigned long time_current, int k){ //모
           lcd.setCursor(0,1);
             lcd.print("  food : 150 g  ");
 
-        int delayvalue = 5000;  //later * 60 * 1000
+        int delayvalue = 5000;  //5000은 임시값, 실제값 : later * 60 * 1000
         static int seconds= delayvalue/1000;
         lcd.setCursor(0,0);
         lcd.print(" seconds : "+String(seconds)+"   ");
@@ -404,13 +326,13 @@ void P_LCD(int period, unsigned long interval){ //주기 설정(주기 스위치
         {lcd.setCursor(0,1);  //lcd출력
         lcd.print("   35  minute   ");
         later = 35;} //delay 변수 설정
-      else if(interval >= 75 and interval <99)  //가변저항
+      else if(interval >= 75 and interval <100)  //가변저항
         {lcd.setCursor(0,1);  //lcd출력
         lcd.print("   45  minute   ");
         later = 45;} //delay 변수 설정
       else
         {lcd.setCursor(0,1);  //lcd출력
-        lcd.print("   e r r o r !  ");}
+        lcd.print("   YXNzIGhvbGU=   ");}
       break;
     }}
 }
@@ -437,13 +359,13 @@ void A_LCD(int amount, unsigned long interval){ //사료량 설정(스위치 on/
         {lcd.setCursor(0,1);  //lcd출력
         lcd.print("     200  g     ");
         mot = 200;}
-      else if(interval >= 75 and interval <99)  //가변저항
+      else if(interval >= 75 and interval <100)  //가변저항
         {lcd.setCursor(0,1);  //lcd출력
         lcd.print("     250  g     ");
         mot = 250;}
       else
         {lcd.setCursor(0,1);  //lcd출력
-        lcd.print("   e r r o r !  ");}
+        lcd.print("  U0VYISEhISEh ");} // easter egg
       break;
     }}
 }
